aba383080423ce7116d28fada176bda16a98845f
ezpaul <46062121+ezpaul@users.noreply.github.com>
2019-03-19 22:52:28 -0400

    Update TodoItem.js
    Replace <tbody> with <li>.

--- src/components/Todo/TodoItem.js
+++ src/components/Todo/TodoItem.js
@@ -32,85 +32,38 @@ class TodoItem extends Component {
     const isDone = todo.completed ? "done" : "undone"
 
     return (
-      <tbody>
-        <tr>
-          {editMode ? (
-            <div>
-              <td>
-                <input
-                  type="text"
-                  value={editText}
-                  onChange={this.onChangeEditText}
-                />
-                <button
-                  type="button"
-                  class="btn-sm btn-outline-primary my-2 my-sm-0"
-                  onClick={this.onSaveEditText}
-                >
-                  <i class="fas fa-save" />{" "}
-                </button>
-                <button
-                  type="button"
-                  class="btn-sm btn-outline-primary my-2 my-sm-0"
-                  onClick={this.onToggleEditMode}
-                >
-                  <i class="fas fa-undo-alt" />{" "}
-                </button>
-              </td>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        {editMode ? (
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-pen-square"></i></span>
+                </div>
+                <input type="text" class="form-control text-md" placeholder="Enter your Todo item" value={editText} onChange={this.onChangeEditText} />
+                <div class="input-group-append">
+                    <button type="button" class="btn-sm btn-outline-primary ml-1" onClick={this.onSaveEditText}><i class="fas fa-save"></i> </button>
+                    <button type="button" class="btn-sm btn-outline-primary ml-1" onClick={this.onToggleEditMode}><i class="fas fa-undo-alt"></i> </button>
+                </div>
             </div>
-          ) : (
+        ) : (
             <div>
-              <td>
                 <label className={isDone}>
-                  <input
+                <input
                     type="checkbox"
+                    className="mr-2"
                     onChange={() => onCheckTodo(todo)}
                     checked={todo.completed}
-                  />
-                  {todo.text} {todo.editedAt && <span>(Edited)</span>}
+                /> {todo.text} {todo.editedAt && <span>(Edited)</span>}
                 </label>
-              </td>
             </div>
-          )}
+        )}
 
-          {/* {editMode ? (
-                    <input type="text" value={editText} onChange={this.onChangeEditText} />
-                ) : (
-                    <span>
-                        <strong>{message.user.username} || {message.user.userId}</strong> {' '}
-                        {message.text} {message.editedAt && <span>(Edited)</span>}
-                    </span>
-                )} */}
-
-          {/* {editMode ? (
-                    <span>
-                        <button type="button" onClick={this.onSaveEditText}>Save</button>
-                        <button type="button" onClick={this.onToggleEditMode}>Cancel</button>
-                    </span>
-                ) : (
-                    <button type="button" onClick={this.onToggleEditMode}>Edit</button>
-                )} */}
-
-          {!editMode && (
-            <td>
-              <button
-                type="button"
-                class="btn-sm btn-outline-primary my-2 my-sm-0"
-                onClick={this.onToggleEditMode}
-              >
-                <i class="fas fa-pen-square" />{" "}
-              </button>{" "}
-              <button
-                type="button"
-                class="btn-sm btn-outline-danger my-2 my-sm-0"
-                onClick={() => onRemoveTodo(todo.uid)}
-              >
-                <i class="fas fa-minus-square" />{" "}
-              </button>
-            </td>
-          )}
-        </tr>
-      </tbody>
+        {!editMode && (
+            <div>
+                <button type="button" class="btn-sm btn-outline-primary ml-1 my-sm-0" onClick={this.onToggleEditMode}><i class="fas fa-pen-square"></i> </button>
+                <button type="button" class="btn-sm btn-outline-danger ml-1 my-sm-0" onClick={() => onRemoveTodo(todo.uid)}><i class="fas fa-minus-square"></i> </button>
+            </div>
+        )}
+    </li>
     )
   }
 }
